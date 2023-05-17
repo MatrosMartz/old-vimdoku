4d37ece72ec20af811e162ccff61913a3816016f
TsonkoAlt <matrosmartz@gmail.com>
2023-05-17 15:08:43 -0600

    chore: add tsconfigPaths for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig, defaultInclude } from 'vitest/config'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [svelte({ hot: !process.env.VITEST })],
+	plugins: [svelte({ hot: !process.env.VITEST }), tsconfigPaths()],
 	test: {
 		include: defaultInclude,
 		environment: 'happy-dom',
