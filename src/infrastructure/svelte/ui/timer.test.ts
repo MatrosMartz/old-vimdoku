4e1d813af619efafd55c27bca3ff472d6bbebb49
TsonkoAlt <matrosmartz@gmail.com>
2023-05-20 14:31:31 -0600

    test: add tests for timer component

---
+++ src/infrastructure/svelte/ui/timer.test.ts
@@ -0,0 +1,45 @@
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, test, vi } from 'vitest'
+import { cleanup, render, screen } from '@testing-library/svelte'
+
+import { hours, minutes, seconds } from '~/tests/utils'
+import { timerStore } from '../stores'
+
+import Timer from './timer.svelte'
+
+describe('Timer Component', () => {
+	let timer: HTMLElement
+
+	beforeAll(() => {
+		vi.useFakeTimers()
+	})
+	beforeEach(() => {
+		cleanup()
+		render(Timer)
+		timerStore.start()
+		timer = screen.getByTestId('timer')
+	})
+	afterEach(() => {
+		timerStore.stop()
+		timerStore.reset()
+		vi.clearAllTimers()
+	})
+	afterAll(() => {
+		vi.useRealTimers()
+	})
+
+	test('Should start with zero', () => {
+		expect(timer).toHaveTextContent('00:00:00')
+	})
+	test('Should display the formatted time value after 5 seconds', async () => {
+		await vi.advanceTimersByTimeAsync(seconds(5))
+		expect(timer).toHaveTextContent('00:00:05')
+	})
+	test('Should display the formatted time value after 10 minutes', async () => {
+		await vi.advanceTimersByTimeAsync(minutes(10))
+		expect(timer).toHaveTextContent('00:10:00')
+	})
+	test('Should display the formatted time value after 1 hour', async () => {
+		await vi.advanceTimersByTimeAsync(hours(1))
+		expect(timer).toHaveTextContent('01:00:00')
+	})
+})
