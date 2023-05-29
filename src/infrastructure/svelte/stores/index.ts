ecee174b251d46fb02af3f741de656c0acc330a9
TsonkoAlt <matrosmartz@gmail.com>
2023-05-29 00:29:02 -0600

    fix: add sudoku store in index store

--- src/infrastructure/svelte/stores/index.ts
+++ src/infrastructure/svelte/stores/index.ts
@@ -1,3 +1,4 @@
-export * from './commands-input.store'
-export * from './modes.store'
-export * from './timer.store'
+export * from './commands-input.store';
+export * from './modes.store';
+export * from './sudoku.store';
+export * from './timer.store';
