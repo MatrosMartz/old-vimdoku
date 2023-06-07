2d09cf0211c4a16f22d342f94b8839886bc366d6
TsonkoAlt <matrosmartz@gmail.com>
2023-06-06 19:56:05 -0600

    fix: add js docs for get methods

--- src/domain/models/history.model.ts
+++ src/domain/models/history.model.ts
@@ -1,6 +1,15 @@
 export interface IHistoryService {
+	/**
+	 * get Autocomplete History
+	 */
 	getAutocompleteHistory: () => string[]
+	/**
+	 * get current value
+	 */
 	getCurrent: () => string | null
+	/**
+	 * get All History
+	 */
 	getHistory: () => string[]
 	push: (cmd: string) => null
 	redo: () => void
