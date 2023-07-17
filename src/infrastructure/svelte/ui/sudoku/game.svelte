4a75f0fbfae74d8277106b556f429827762e860e
TsonkoAlt <matrosmartz@gmail.com>
2023-07-16 19:13:58 -0600

    refactor: separate numbers bar component

--- src/infrastructure/svelte/ui/sudoku/game.svelte
+++ src/infrastructure/svelte/ui/sudoku/game.svelte
@@ -4,17 +4,19 @@
 	import NumberBar from './number-bar.svelte'
 	import Board from './board.svelte'
 
-	$: numbersBars = $settingsStore.numbers || $settingsStore.relativeNumbers ? '3ch' : '0'
+	$: hasBars = $settingsStore.numbers || $settingsStore.relativeNumbers
 </script>
 
 {#if $boardStore !== false}
 	<section
 		in:blur
-		class="game bg-surface-100-800-token rounded-lg overflow-hidden gap-4 p-2"
-		style:--numbers={numbersBars}
+		class="relative bg-surface-100-800-token rounded-lg gap-0 p-2 transition-[border-radius]"
+		class:has-bars={hasBars}
 	>
-		<NumberBar direction="col" />
-		<NumberBar direction="row" />
+		{#if hasBars}
+			<NumberBar direction="col" />
+			<NumberBar direction="row" />
+		{/if}
 		<Board />
 	</section>
 {:else}
@@ -24,14 +26,7 @@
 {/if}
 
 <style lang="postcss">
-	.game {
-		display: grid;
-		grid-template-areas:
-			'. c'
-			'r b';
-
-		grid-template-columns: var(--numbers) 1fr;
-		grid-template-rows: var(--numbers) 1fr;
-		transition: all 500ms ease-in-out;
+	.has-bars {
+		@apply rounded-t-none rounded-l-none;
 	}
 </style>
